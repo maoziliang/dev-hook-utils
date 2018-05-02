a803d3cabd65f25f2e76c250c84c070b418eb54c
maoziliang <mozily@qq.com>
2018-05-02 11:33:20 +0800

    增加对没有配置commit.template的情景的支持

--- src/hooks/hook-prepareMsg.js
+++ src/hooks/hook-prepareMsg.js
@@ -3,10 +3,10 @@ const editMsgFile = args[0];
 const rawMsg = args[1];
 
 export function run() {
-  // if raw command is `git commit`, the rawMsg argument is 'template'.
+  // if raw command is `git commit`, the rawMsg argument is 'template'(commit.template configed) or undefined(no commit.template config).
   // if raw command is `git commit --amend`, the rawMsg argument is 'commit'.
   // Here, we hook the `git commit` command to provide commitizen ui to write commit message.
-  if (rawMsg === 'template') {
+  if (rawMsg === 'template' || rawMsg === undefined) {
     const prompt = require('../commitizen').default;
     prompt(editMsgFile);
   }
